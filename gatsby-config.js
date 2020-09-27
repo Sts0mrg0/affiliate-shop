e417d6d9d252860ddfe9b364d9ab25379884a423
jesus <jgc.developments@gmail.com>
2020-09-27 09:19:58 +0200

    new page /todos added with graphql query

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,22 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/data`,
+      },
+    },
+  ],
 }
---
+++ src/pages/todos.jsx
@@ -0,0 +1,26 @@
+import { graphql } from "gatsby"
+import React from "react"
+import Layout from "../components/layout"
+
+export default props => {
+  const todos = props.data.allTodosJson.nodes
+  return <Layout>
+    <ul>
+      {
+        todos.map(todo => <li key={todo.id}> {todo.description} </li>)
+      }
+    </ul>
+  </Layout>
+}
+
+export const query = graphql`
+  {
+    allTodosJson {
+      nodes {
+        description
+        id
+        points
+      }
+    }
+  }
+`
