d5fca99413162e62ad3dfcc0987c7d3b2c728800
jesus <jgc.developments@gmail.com>
2020-09-27 17:05:56 +0200

    Adding header and footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,13 @@
 import React from "react"
+import HeaderLayout from "../components/HeaderLayout"
+import FooterLayout from "./FooterLayout"
+
 export default function Layout({ children }) {
   return (
     <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
+      <HeaderLayout />
       {children}
+      <FooterLayout />
     </div>
   )
 }
